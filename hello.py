a3c58b87822054dada3403b5b445ae5e72d90fd0
shenmj053 <shenmj053@163.com>
2016-04-23 00:21:41 +0800

    c6

--- hello.py
+++ hello.py
@@ -6,10 +6,15 @@ from flask import session
 from flask import redirect
 from flask import url_for
 from flask import flash
+from threading import Thread
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Shell
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
 #from flask import request  #请求上下文
 #from flask import redirect  #重定向
 from datetime import datetime
@@ -17,6 +22,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -24,11 +30,37 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWM'] = True
+#邮箱配置
+app.config['MAIL_SERVER'] = 'stmp.163.com'
+app.config['MAIL_PORT'] = '25'
+app.config['MAIL_USE_TLS'] = True
+#app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+#app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin shenmj053@163.com'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app,msg])
+    thr.start()
+    return thr
+
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
@@ -38,12 +70,20 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():  #验证所提交的数据
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)  #将user加入数据库会话中
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'new user', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
         session['name'] = form.name.data  #将数据存储在用户会话中
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())  #使用模板,加入datetime变量
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False), current_time=datetime.utcnow())  #使用模板,加入datetime变量
     #return redirect ('https://www.baidu.com')  #重定向
     #user_agent = request.headers.get('User-Agent')
     #return '<p>Your Browser is %s</p>' % user-went
@@ -65,7 +105,7 @@ class Role(db.Model):
     __tablename__= 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -80,7 +120,10 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 
 
