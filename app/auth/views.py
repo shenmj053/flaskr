cf17ac9a33d46ac890b3abdf85b241ea0674ed31
shenmj053 <shenmj053@163.com>
2016-04-28 00:12:54 +0800

    add register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,42 @@
-from flask import render_template
+#_*_coding: utf-8 _*_
+from datetime import datetime
+from flask import render_template, redirect, request, url_for, flash
+from flask.ext.login import login_user
+from flask.ext.login import logout_user, login_required
 from . import auth
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
 
-@auth.route('/login')
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form  = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form, current_time=datetime.utcnow())
+
+
+
+
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()  #用表单中填写的email从数据库中加载用户
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get(next) or url_for('main.index'))
+        flash('Invalid username or password')
+    return render_template('auth/login.html', form=form, current_time=datetime.utcnow())
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class Role(db.Model):
 class User(UserMixin, db.Model):
     __tablename__= 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Colunm(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))  #密码散列值
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
